add787b85eaaab5b45f8cd21b5106131a1112862
taeh <taeh@pc3-034-l.cs.st-andrews.ac.uk>
2018-04-25 23:51:28 +0100

    Fixed tweetCount; Implemented fast (Big O) top x hashtag counter

--- parsing.py
+++ parsing.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from operator import itemgetter
+import json
+from collections import Counter
 
 class CensusReader:
     def __init__(self, fileName):
@@ -8,6 +11,13 @@ class CensusReader:
 
         self.data["valid"] = True
         self.validateFile()
+
+    def removeDuplicates(self, seq):
+        # Not order preserving
+        keys = {}
+        for e in seq:
+            keys[e] = 1
+        return keys.keys()
 
     def validateFile(self):
         #check duplication
@@ -37,7 +47,7 @@ class CensusReader:
     def retweetCount(self):
         texts = self.data.loc[:,"text"]
         counts = 0
-        for i, v in texts.iteritems():
+        for v in texts.iteritems():
             if type(v) is not str:
                 continue
             if(v[0:4] == "RT @"):
@@ -45,20 +55,48 @@ class CensusReader:
         return counts
 
     def replyCount(self):
-        texts = self.data.loc[:, "in_reply_to_screen_name"]
+        replytweets = self.data.loc[:, "in_reply_to_screen_name"]
         counts = 0
-        for v in texts:
+        for v in replytweets:
             if type(v) is str:
                 counts += 1
         return counts
 
     def tweetCount(self):
-        result = len(self.data) - self.retweetCount() - self.replyCount()
+        result = len(self.data.loc[:, 'entities_str'])
         return result
 
-    def numOfUsers(self):
-        newData = self.data.drop_duplicates(subset=['from_user_id_str'])
-        print(len(newData))
+    def userCount(self):
+        tweetsWithUniqueUsers = self.data.drop_duplicates(subset=['from_user_id_str'])
+        return len(tweetsWithUniqueUsers)
+    
+    def getHashTags(self):
+        #     hashtags = self.data.groupby('entities_str').count().sort_values(by=['id_str'], ascending=False).head(number)
+        data = self.data
+        hashtags = []
+        jsonItems = data.loc[:, 'entities_str'] # lst of JSON hashtag items
+        # each JSON hashtag item has syntax: {""hashtags"":[{""text"":""67P"",""indices"":[58,62]},{""text"":""CometWatch"",""indices"":[127,138]},{""text"":""CometLanding"",""indices"":[139,140]}],""symbols"":[],""user_mentions"":[{""screen_name"":""ESA_Rosetta"",""name"":""ESA Rosetta Mission"",""id"":253536357,""id_str"":""253536357"",""indices"":[3,15]}],""urls"":[{""url"":""http://t.co/Z2A14Jorv6"",""expanded_url"":""http://youtu.be/4a3eY5siRRk"",""display_url"":""youtu.be/4a3eY5siRRk"",""indices"":[104,126]}]}".
+        for JSONString in jsonItems:
+            if not type(JSONString) is str: continue
+            parsedJSON = json.loads(JSONString)
+            JSONHashTagsElement = parsedJSON['hashtags']
+            for JSONHashTag in JSONHashTagsElement:
+                hashtags.append(JSONHashTag['text'])
+        return hashtags
+    
+    def getHTagsAndCounts(self):
+        hashtags = sorted(self.getHashTags(), reverse=False)
+        hashtagsNoDuplicates = self.removeDuplicates(hashtags)
+        hTagsAndCounts = []
+        counter = Counter(hashtags)
+        for ht in hashtagsNoDuplicates:
+            count = counter[ht] # hashtags.count(ht)
+            hTagsAndCounts.append([ht, count])
+        return hTagsAndCounts
 
-    def mostPopHashtags(self):
-        print(self.data.groupby('entities_str').count().sort_values(by=['id_str'], ascending=False).head(1))
+    def mostPopHashtags(self, number):
+        sortedHTagsAndCounts = sorted(self.getHTagsAndCounts(), key = itemgetter(1), reverse=True)
+        returnString = ""
+        for x in range(0,number):
+            returnString += str(x + 1) + ". #" + sortedHTagsAndCounts[x][0] + " : " + str(sortedHTagsAndCounts[x][1]) + " tweets (of any type)\n"
+        return returnString
