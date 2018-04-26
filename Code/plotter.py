561c5bc31f883e5a37ae1b7f2d0cac866eab567f
taeh <taeh@pc3-040-l.cs.st-andrews.ac.uk>
2018-04-26 17:56:47 +0100

    Started on network graph

--- Code/plotter.py
+++ Code/plotter.py
@@ -5,9 +5,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ipywidgets
 from matplotlib import ticker
-
+import networkx as nx
 
 class Plotter:
+
+    def removeDuplicates(self, seq):
+        # Not order preserving
+        keys = {}
+        for e in seq:
+            keys[e] = 1
+        return keys.keys()
 
     def pieChart(self, tweets, retweets, replies):
         total = tweets + retweets + replies
@@ -138,6 +145,49 @@ class Plotter:
         plt.axis("off")
         plt.show()
     
-    def networkGraph(self, data):
-        print("I'm a graph lol")
+    def repliesToTwoUsers(self, data):
+        interactions = []
+        for interaction in data:
+            sentBy = interaction['from_user']
+            inReplyTo = interaction['in_reply_to_screen_name']
+            interactions.append([sentBy, inReplyTo])
+        return self.removeDuplicates(sorted(interactions, reverse=False))
+
+    def retweetsToTwoUsers(self, data):
+        interactions = []
+        for interaction in data:
+            sentBy = interaction['from_user']
+            inReplyTo = interaction['entities_str']['user_mentions'][0]['screen_name']
+            interactions.append([sentBy, inReplyTo])
+        return self.removeDuplicates(sorted(interactions, reverse=False))
     
+    def getInteractionUsers(self, data):
+        # given list of tweets
+        # return list of interactions as 2d array
+        # each item contains the 2 users involved in the interaction
+        replies = data[data['in_reply_to_screen_name'].notnull() == True]
+        retweets = data[data['text'].str.startswith("RT") == True]
+        formattedRetweets = self.retweetsToTwoUsers(sorted(retweets, reverse=False))
+        formattedReplies = self.repliesToTwoUsers(sorted(replies, reverse=False))
+        return self.removeDuplicates(sorted(formattedReplies + formattedRetweets, reverse=False))
+    
+    def networkGraph(self, data):
+        # draw each user as a node, each interaction as an edge; eliminate non-interacting users
+        print("I'm a graph lol\n\n")
+        G = nx.DiGraph()
+        interactionUsers = self.getInteractionUsers(data)
+
+        for twoUsers in interactionUsers:
+            G.add_edge(twoUsers[0], twoUsers[1])
+        UG = G.to_undirected()
+
+        options = {
+            'node_color': 'black',
+            'node_size': 1,
+            'line_color': 'grey',
+            'linewidths': 0,
+            'width': 0.1,
+        }
+
+        nx.draw_circular(UG, **options)
+        plt.show()
